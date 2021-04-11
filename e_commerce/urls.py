43b079ea55fdefd23019002e1536ac456528b3a0
Junior <juniorvolupia@yahoo.com.br>
2021-04-11 15:03:56 -0300

    inserindo a lista

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 
+from products.views import ProductListView, product_list_view
 from django.contrib import admin
 from django.urls import path
 from .views import home_page,about_page,contact_page,login_page,register_page
@@ -26,6 +27,8 @@ urlpatterns = [
     path('contact',contact_page),
     path('login/', login_page),
     path('register/', register_page),
+    path('products/', ProductListView.as_view()),
+    path('products-fbv/', product_list_view),  
     path('admin/', admin.site.urls),
 ]
 if settings.DEBUG:
