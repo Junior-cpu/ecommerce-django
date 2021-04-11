68696f23fffd6561608c78fbaff4399e3aebdfb1
Junior <juniorvolupia@yahoo.com.br>
2021-04-11 19:25:48 -0300

    inserindo pagina details

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 
-from products.views import ProductListView, product_list_view
+from products.views import ProductListView, product_list_view,ProductDetailView, product_detail_view
 from django.contrib import admin
 from django.urls import path
 from .views import home_page,about_page,contact_page,login_page,register_page
@@ -29,6 +29,8 @@ urlpatterns = [
     path('register/', register_page),
     path('products/', ProductListView.as_view()),
     path('products-fbv/', product_list_view),  
+    path('products/<int:pk>', ProductDetailView.as_view()),
+    path('products-fbv/<int:pk>', product_detail_view), 
     path('admin/', admin.site.urls),
 ]
 if settings.DEBUG:
--- products/views.py
+++ products/views.py
@@ -1,17 +1,18 @@
-from django.shortcuts import render
-from django.views.generic import ListView
-# Create your views here.
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render, get_object_or_404
+
 from .models import Product
+
 #Class Based View
 class ProductListView(ListView):
     #traz todos os produtos do banco de dados sem filtrar nada 
     queryset = Product.objects.all()
     template_name = "products/list.html"
-
-   # def get_context_data(self, *args, **kwargs):
-       # context = super(ProductListView, self).get_context_data(*args, **kwargs)
-      #  print(context)
-      #  return context
+    
+    #def get_context_data(self):
+        #context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
+        #print(context)
+        #return context
 
 #Function Based View
 def product_list_view(request):
@@ -20,3 +21,23 @@ def product_list_view(request):
         'object_list': queryset
     }
     return render(request, "products/list.html", context)
+
+#Class Based View
+class ProductDetailView(DetailView):
+    #traz todos os produtos do banco de dados sem filtrar nada 
+    queryset = Product.objects.all()
+    template_name = "products/detail.html"
+    
+    def get_context_data(self):
+        context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
+        print(context)
+        return context
+
+#Function Based View
+def product_detail_view(request, pk = None, *args, **kwargs):
+    #instance = Product.objects.get(pk = pk) #get the object id
+    instance = get_object_or_404(Product, pk = pk)
+    context = {
+        'object': instance
+    }
+    return render(request, "products/detail.html", context)
