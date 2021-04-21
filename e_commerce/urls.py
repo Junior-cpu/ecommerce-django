4d297ffdf2ef85295da9a2770e00d03320bbc622
Junior <juniorvolupia@yahoo.com.br>
2021-04-21 12:46:37 -0300

    alterando view,model ,urls

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 
-from products.views import ProductListView, product_list_view,ProductDetailView, product_detail_view
+from products.views import ProductListView, product_list_view,ProductDetailView, product_detail_view,  ProductFeaturedListView,ProductFeaturedDetailView
 from django.contrib import admin
 from django.urls import path
 from .views import home_page,about_page,contact_page,login_page,register_page
@@ -27,6 +27,8 @@ urlpatterns = [
     path('contact',contact_page),
     path('login/', login_page),
     path('register/', register_page),
+    path('featured/', ProductFeaturedListView.as_view()),
+    path('featured/<int:pk>/', ProductFeaturedDetailView.as_view()),
     path('products/', ProductListView.as_view()),
     path('products-fbv/', product_list_view),  
     path('products/<int:pk>', ProductDetailView.as_view()),
