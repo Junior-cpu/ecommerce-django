dcd72b3659be2d9b8365d50af3b46b2788338531
Junior <juniorvolupia@yahoo.com.br>
2021-04-23 20:22:07 -0300

    inserindo slug à aplicação

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 
-from products.views import ProductListView, product_list_view,ProductDetailView, product_detail_view,  ProductFeaturedListView,ProductFeaturedDetailView
+from products.views import ProductListView, product_list_view,ProductDetailView, product_detail_view,  ProductFeaturedListView,ProductFeaturedDetailView, ProductDetailSlugView
 from django.contrib import admin
 from django.urls import path
 from .views import home_page,about_page,contact_page,login_page,register_page
@@ -32,7 +32,8 @@ urlpatterns = [
     path('products/', ProductListView.as_view()),
     path('products-fbv/', product_list_view),  
     path('products/<int:pk>', ProductDetailView.as_view()),
-    path('products-fbv/<int:pk>', product_detail_view), 
+    path('products-fbv/<int:pk>', product_detail_view),
+    path('products/<slug:slug>/', ProductDetailSlugView.as_view()), 
     path('admin/', admin.site.urls),
 ]
 if settings.DEBUG:
